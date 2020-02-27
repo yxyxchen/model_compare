68681c650d7ab37defe9e44f508f4383555c9023
Yixin <yxyxchen@bu.edu>
2020-02-27 14:12:31 -0500

    daily

--- modelComparison.m
+++ modelComparison.m
@@ -1,15 +1,15 @@
-logEvidenceList = csvread('logEvidenceListCV.csv');
+logEvidenceList = csvread('logEvidenceList.csv');
 L = logEvidenceList';
-L = L(1:5,:);
+L = L(1:3,:);
 [posterior,out] = VBA_groupBMC(L);
 
 out.Ef
 out.ep
 
 
-logEvidenceList = csvread('logEvidenceListCV.csv');
+logEvidenceList = csvread('logEvidenceList.csv');
 L = logEvidenceList';
-L = L(1:5,L(6,:) == 2)
+L = L(1:2,57 : 101)
 [posterior,out] = VBA_groupBMC(L) ;
 
 out.Ef
